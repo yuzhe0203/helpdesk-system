dc26ac9a064a339f277fb22d1fb20615a722fa67
yuche <mick930203@gmail.com>
2026-02-09 17:06:59 +0800

    chore: add users module and base user service

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { User, UserRole } from '@prisma/client';
+
+@Injectable()
+export class UsersService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async findByEmail(email: string): Promise<User | null> {
+        return this.prisma.user.findUnique({
+            where: { email },
+        });
+    }
+
+    async createUser(email: string, passwordHash: string): Promise<User> {
+        return this.prisma.user.create({
+            data: {
+                email,
+                passwordHash,
+                role: UserRole.USER,
+            },
+        });
+    }
+
+}
